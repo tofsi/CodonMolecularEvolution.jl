f45241353194b8f008e1574796d4fca30e2a8f9c
Tove Nordmark <tove.nordmark.01@gmail.com>
2025-07-13 19:42:49 +0200

    trying to fix the goddamned branches

--- src/difFUBAR/skbDifFUBAR.jl
+++ src/difFUBAR/skbDifFUBAR.jl
@@ -21,7 +21,7 @@ struct GeneralizedFUBARModel
 end
 
 function log_posterior(model::GeneralizedFUBARModel, parameters::Vector{Float64})
-    return model.log_likelihood(model.parameters_to_probability_vector(parameters)) + model.log_prior(parameters)
+    return model.log_likelihood(model.parameters_to_probability_vector(parameters)) + model.prior.log_pdf(parameters)
 end
 
 """
@@ -110,7 +110,7 @@ function SKBDIModel(parameter_grids::Vector{Vector{Float64}},
     ambient_to_parameter_transform::Function,
     kernel_dim::Int64)
     masks, mask_subset_indicators = gen_disjoint_masks(hypothesis_masks)
-    suppression_dim = size(masks)[1]
+    suppression_dim = size(hypothesis_masks)[1]
     unsuppressed_dim = size(log_con_lik_matrix)[1]
     total_dim = kernel_dim + suppression_dim + unsuppressed_dim
 
@@ -147,15 +147,19 @@ function to_probability_vector(model::SKBDIModel, ambient_sample::Vector{Float64
     Computes the probability vector for the model given the parameters.
     parameters start with kernel parameters, followed by suppression parameters, 
     and then unsuppressed parameters.
+    TODO: we wanna return the minimum of suppression params in case of overlap probably, not multiplying them
     """
     parameters = model.ambient_to_parameter_transform(ambient_sample)
     _, suppression_parameters, unsuppressed_parameters = split_parameters(model, parameters)
     probability_vector = softmax(unsuppressed_parameters)
     # Apply the transition functions for each suppression parameter
-    for i in 1:model.suppression_dim
-        probability_vector[model.masks[i, :]] .*= model.transition_function(suppression_parameters[i])
+    transition_function_values = model.transition_function.(suppression_parameters)
+    for i in 1:size(model.masks, 1)
+        # In the case of hypothesis overlap, we take the minimum of the transition functions
+        probability_vector[model.masks[i, :]] .*= minimum(
+            transition_function_values[model.mask_subset_indicators[i, :]])
     end
-    return probability_vector ./ sum(probability_vector)
+    return any(probability_vector .> 0.0) ? probability_vector ./ sum(probability_vector) : probability_vector
 end
 
 function log_likelihood(model::SKBDIModel, ambient_sample::Vector{Float64})
@@ -203,7 +207,9 @@ function generate_square_l2_distance_matrix(codon_param_index_vec::Vector{Vector
     return square_distance_matrix
 end
 
-function calculate_alloc_grid_and_theta(model::GeneralizedFUBARModel, ambient_samples::Vector{Vector{Float64}}, burnin::Int)
+function calculate_alloc_grid_and_theta(model::GeneralizedFUBARModel,
+    ambient_samples::Vector{Vector{Float64}},
+    burnin::Int, progress=false)
     """
     Samples the allocation grid from the ambient samples
     TODO: WHY DOES THIS TAKE SO MUCH TIME x_x
@@ -213,7 +219,8 @@ function calculate_alloc_grid_and_theta(model::GeneralizedFUBARModel, ambient_sa
     alloc_grid = zeros(Int64, size(model.con_lik_matrix))
     theta = zeros(Float64, model.n_categories)
     println()
-    @showprogress for i = burnin+1:n_samples
+    p = progress ? Progress(n_samples - burnin; desc="Sampling allocations") : nothing
+    for i = burnin+1:n_samples
         probability_vector = model.to_probability_vector(ambient_samples[i])
         theta .+= probability_vector # Accumulate the probability vector
         v = zeros(Float64, model.n_categories)
@@ -224,15 +231,16 @@ function calculate_alloc_grid_and_theta(model::GeneralizedFUBARModel, ambient_sa
             samp = sample(1:model.n_categories, Weights(v))
             alloc_grid[samp, i] += 1 # Increment the allocation for the sampled category
         end
+        progress ? next!(p) : nothing
     end
     return alloc_grid, theta ./ (n_samples - burnin)
 end
 
 
 
-function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
-    tag_colors; pos_thresh=0.95, iters=2500,
-    burnin::Int=div(iters, 5), concentration=0.1, binarize=false, verbosity=1,
+function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath;
+    tag_colors=CodonMolecularEvolution.DIFFUBAR_TAG_COLORS[sortperm(tags)], pos_thresh=0.95, iters=5000,
+    burnin::Int=0, concentration=0.1, binarize=false, verbosity=1,
     exports=true, exports2json=false, code=MolecularEvolution.universal_code,
     optimize_branch_lengths=false, version=nothing, t=0)
     # TODO: Split this up into multiple fcts because the setup is mostly shared with difFUBAR
@@ -240,41 +248,27 @@ function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
         analysis_name = outpath
         leaf_name_transform = CodonMolecularEvolution.generate_tag_stripper(tags)
         plot_collection = NamedTuple[]
-        tree, tags, tag_colors, analysis_name = CodonMolecularEvolution.difFUBAR_init(analysis_name,
-            treestring,
-            tags,
-            tag_colors=tag_colors,
-            exports=exports,
-            verbosity=verbosity,
-            disable_binarize=!binarize, plot_collection=plot_collection)
+        tree, tags, tag_colors, analysis_name = CodonMolecularEvolution.difFUBAR_init(
+            analysis_name, treestring, tags, tag_colors=tag_colors, exports=exports,
+            verbosity=verbosity, disable_binarize=!binarize, plot_collection=plot_collection)
         ((tree, LL, alpha, beta, GTRmat, F3x4_freqs, eq_freqs), fit_time) =
-            @timed CodonMolecularEvolution.difFUBAR_global_fit_2steps(seqnames,
-                seqs,
-                tree,
-                leaf_name_transform,
-                code,
-                verbosity=verbosity,
+            @timed CodonMolecularEvolution.difFUBAR_global_fit_2steps(
+                seqnames, seqs, tree, leaf_name_transform, code, verbosity=verbosity,
                 optimize_branch_lengths=optimize_branch_lengths)
-
-        ((con_lik_matrix, log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid, param_kinds, shallow_tree, background_omega_grid, codon_param_index_vec), grid_time) =
-            @timed CodonMolecularEvolution.difFUBAR_grid(tree,
-                tags,
-                GTRmat,
-                F3x4_freqs,
-                code,
-                verbosity=verbosity,
-                foreground_grid=4,
-                background_grid=2,
-                version=version,
-                t=t)
+        ((con_lik_matrix, log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid,
+                param_kinds, shallow_tree, background_omega_grid, codon_param_index_vec), grid_time) =
+            @timed CodonMolecularEvolution.difFUBAR_grid(
+                tree, tags, GTRmat, F3x4_freqs, code, verbosity=verbosity,
+                foreground_grid=4, background_grid=2, version=version, t=t)
         # We should figure out a good prior dist for F(s)
         transition_function = s -> CodonMolecularEvolution.quintic_smooth_transition(s, -1, 1)
         # Define the masks for the suppression parameters
+        # TODO: Make this an argument to this function
         hypothesis_masks = ones(Bool, (4, size(con_lik_matrix)[1]))
         hypothesis_masks[1, :] = [c[2] > 1 for c in codon_param_vec] # omega_1 > 1
         hypothesis_masks[2, :] = [c[3] > 1 for c in codon_param_vec] # omega_2 > 1
-        hypothesis_masks[2, :] = [c[2] > c[3] for c in codon_param_vec] # omega_1 > omega_2
-        hypothesis_masks[3, :] = [c[3] > c[2] for c in codon_param_vec] # omega_2 > omega_1
+        hypothesis_masks[3, :] = [c[2] > c[3] for c in codon_param_vec] # omega_1 > omega_2
+        hypothesis_masks[4, :] = [c[3] > c[2] for c in codon_param_vec] # omega_2 > omega_1
 
         square_distance_matrix = generate_square_l2_distance_matrix(codon_param_index_vec)
         kernel_stddev = 1.0 # example values idk what these should be xD
@@ -299,14 +293,24 @@ function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
         # One further step of abstraction skbdi -> generalized fubar
         fubar_model = GeneralizedFUBARModel(model)
         ess_model = ESSModel(fubar_model.prior, fubar_model.log_likelihood)
+        if verbosity > 0
+            println("Step 4: Sampling from the model.")
+        end
         sample_time = @elapsed begin
             ambient_samples = AbstractMCMC.sample(ess_model, ESS(), iters, progress=true)
-            alloc_grid, theta = calculate_alloc_grid_and_theta(fubar_model, ambient_samples, burnin)
+            alloc_grid, theta = calculate_alloc_grid_and_theta(fubar_model, ambient_samples, burnin, progress=verbosity > 0)
         end
     end
     # Now we should have the same stuff that diffubar generates.
-    df, plots_named_tuple = CodonMolecularEvolution.difFUBAR_tabulate_and_plot(analysis_name, pos_thresh, alloc_grid, codon_param_vec, alphagrid, omegagrid, tag_colors, verbosity=verbosity, exports=exports)
-    json = CodonMolecularEvolution.dNdS2JSON(CodonMolecularEvolution.difFUBAR2JSON(), (outpath=analysis_name, df=df, θ=theta, posterior_mat=alloc_grid ./ sum(alloc_grid, dims=1), categories=reduce(hcat, codon_param_vec)', tags=tags, tree=shallow_tree, LL=LL, timers=(total_time, fit_time, grid_time, sample_time), treestring=treestring, seqnames=seqnames, seqs=seqs, leaf_name_transform=leaf_name_transform, pos_thresh=pos_thresh, iters=iters, burnin=burnin, concentration=concentration, binarize=binarize, exports=exports2json))
+    df, plots_named_tuple = CodonMolecularEvolution.difFUBAR_tabulate_and_plot(
+        analysis_name, pos_thresh, alloc_grid, codon_param_vec, alphagrid, omegagrid, tag_colors, verbosity=verbosity, exports=exports)
+    json = CodonMolecularEvolution.dNdS2JSON(CodonMolecularEvolution.difFUBAR2JSON(),
+        (outpath=analysis_name, df=df, θ=theta, posterior_mat=alloc_grid ./ sum(alloc_grid, dims=1),
+            categories=reduce(hcat, codon_param_vec)', tags=tags, tree=shallow_tree, LL=LL,
+            timers=(total_time, fit_time, grid_time, sample_time),
+            treestring=treestring, seqnames=seqnames, seqs=seqs,
+            leaf_name_transform=leaf_name_transform, pos_thresh=pos_thresh,
+            iters=iters, burnin=burnin, concentration=concentration, binarize=binarize, exports=exports2json))
     #Return df, (tuple of partial calculations needed to re-run tablulate), plots
     push!(plot_collection, plots_named_tuple)
     return df, (alloc_grid, ambient_samples, model, tag_colors), merge(plot_collection...)
@@ -315,11 +319,13 @@ end
 analysis_name = "output/Ace2"
 seqnames, seqs = read_fasta("test/data/Ace2_tiny/Ace2_tiny_tagged.fasta");
 treestring, tags, tag_colors = import_colored_figtree_nexus_as_tagged_tree("test/data/Ace2_tiny/Ace2_tiny_tagged.nex")
-df, results = skbdifFUBAR(seqnames, seqs, treestring, tags, analysis_name, tag_colors)
-alloc_grid, ambient_samples, output_grid, _ = results
+df, results = skbdifFUBAR(seqnames, seqs, treestring, tags, analysis_name)
+alloc_grid, ambient_samples, model, _ = results
+model.transition_function = nothing # Problems loading and saving anonymous functions
+
 @save "output/alloc_grid.jld2" alloc_grid
 @save "output/ambient_samples.jld2" ambient_samples
-@save "output/grid.jld2" output_grid
+@save "output/grid.jld2" model
 
 
 
